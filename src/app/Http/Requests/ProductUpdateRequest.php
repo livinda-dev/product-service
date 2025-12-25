81dcec4927d4ee9e63b9366cb20eed361a79e2ac
livinda-dev <sounsok626@gmail.com>
2025-12-25 21:19:20 +0700

    fix base64 error

--- src/app/Http/Requests/ProductUpdateRequest.php
+++ src/app/Http/Requests/ProductUpdateRequest.php
@@ -7,10 +7,10 @@ use Illuminate\Foundation\Http\FormRequest;
 class ProductUpdateRequest extends FormRequest
 {
 
-    public function authorize(): bool
-    {
-        return true;
-    }
+    // public function authorize(): bool
+    // {
+    //     return true;
+    // }
     public function rules()
 {
     return [
