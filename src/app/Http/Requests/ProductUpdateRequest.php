078e036d7fd695d42661a36fa2231c2dadeae275
livinda-dev <sounsok626@gmail.com>
2025-12-25 23:45:53 +0700

    Update ProductUpdateRequest.php

--- src/app/Http/Requests/ProductUpdateRequest.php
+++ src/app/Http/Requests/ProductUpdateRequest.php
@@ -6,11 +6,6 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class ProductUpdateRequest extends FormRequest
 {
-
-    // public function authorize(): bool
-    // {
-    //     return true;
-    // }
     public function rules()
 {
     return [
@@ -20,8 +15,7 @@ class ProductUpdateRequest extends FormRequest
         'price' => 'sometimes|numeric|min:0',
         'stock' => 'sometimes|integer|min:0',
         'is_active' => 'sometimes|boolean',
-        'images' => 'present|array',
-        'images.*' => 'required|string',
+        'image' => 'nullable|string', // base64
     ];
 }
 
